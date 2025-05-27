2efce6e64d96a31dcd29c1d32902ee838892b03d
Yogesh Sharma <yogesh@lovelace.flair.cds.iisc>
2025-05-27 12:31:15 +0530

    fixed seed error for data shuffling

--- tuned_lens/scripts/train_loop.py
+++ tuned_lens/scripts/train_loop.py
@@ -334,7 +334,7 @@ class Train:
 
         logger.debug(f"Creating data loader and setting seed to {self.seed} ...")
         dl = self.dist.dataloader(data)
-        dl.seed(self.seed)
+        th.manual_seed(self.seed)
         logger.debug("Creating optimizer and scheduler ...")
         params = [p for p in lens.parameters() if p.requires_grad]
         opt = self.opt.create_optim(params)
